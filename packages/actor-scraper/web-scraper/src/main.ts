abd8e4032ff7f36e979635292225f251ff000331
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-05-06 11:35:52 +0530

    Update main.ts

--- packages/actor-scraper/web-scraper/src/main.ts
+++ packages/actor-scraper/web-scraper/src/main.ts
@@ -1,15 +1,5 @@
 import { runActor } from '@apify/scraper-tools';
 import { CrawlerSetup } from './internals/crawler_setup.js';
-//import { Apify } from 'apify';
-import * as Apify from 'apify';
 
 
 runActor(CrawlerSetup);
-
-//const Apify = require('dfdfk/apify');
-
-Apify.main(async () => {
-    const run = await Apify.call('apify/hello-world', { greeting: 'Hello sir!' });
-    console.log('Apify.call() result:');
-    console.dir(run);
-});
